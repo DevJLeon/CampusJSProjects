11ab00856adb36b5133946cbf3ad74dc575e22d6
Joshua <celisjoshleon@gmail.com>
2023-05-31 14:21:05 -0500

    a

--- P02/matrix.js
+++ P02/matrix.js
@@ -3,7 +3,7 @@ let matrix = [];
 let sortedMatrix =  new Array;
 
 let m = 3;
-let n = Math.floor(Math.random() * 5);
+let n = 4//Math.floor(Math.random() * 5);
 
 // BUILDING THE MATRIX (randomized)
 
@@ -22,7 +22,7 @@ const buildMat = (m=3,n=Math.floor(Math.random() * 10)) =>{
 for (let i = 0; i<m; i++){
     matrix[i] = Array(n);
     for (let j = 0;j <n; j++){
-        matrix[i][j] = Math.floor(Math.random() * 10);
+        matrix[i][j] = Math.floor(Math.random() * 101);
     };
 };
 
@@ -33,24 +33,31 @@ let superMat = [];
 for (let k = 0; k<m; k++){
     superMat.push(...matrix[k])
 };
+console.log(superMat);
 
 superMat.sort((a, b) => a - b); //sorting the supermatrix
 
+
+
 for (let i = 0; i<m;i++){
-    sortedMatrix[i] = Array(n);
+    sortedMatrix[i] = Array(n); // creates empty matrix
     if (i%2 == 0){
-        for (let x = 0; x<n; x++){
+        for (let x = 0; x<n; x++){ //add numbers to even rows
             for (let j = 0;j <n; j++){
+              console.log(i)
+                sortedMatrix[i][j] = superMat.shift();
                 
-                sortedMatrix[i][j] ;
             };
         };
     };
 };
 
-
-//BODY
-sortedMatrix.splice(0[0],0[0],"hola") 
-console.log(sortedMatrix)
+//BODY 
 console.log(matrix);
 console.log(superMat);
+console.log(sortedMatrix)
+sortedMatrix[0][0] = superMat.shift();
+sortedMatrix[0][1] = superMat.shift();
+sortedMatrix[0][2] = superMat.shift();
+sortedMatrix[0][3] = superMat.shift();
+console.log(sortedMatrix)
