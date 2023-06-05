b23634d3b58aecaadadd490f839d1973f76919d6
Joshua <celisjoshleon@gmail.com>
2023-06-05 17:40:16 -0500

    movie project

---
+++ map/maps.js
@@ -0,0 +1,40 @@
+/*
+const mapaSaur = new Map;
+
+// MAP METHODS
+mapaSaur.clear(); //remove all key-value
+mapaSaur.delete(); //Validates if an element existed and was removed.
+mapaSaur.entries(); //
+//mapaSaur.forEach(); // does something for each pair in the map.
+mapaSaur.get(); // returns a value associated to a key.
+mapaSaur.has(); // Validates if a key has an associated value.
+mapaSaur.keys(); // returns all the keys from the object
+mapaSaur.values(); // returns all the values from the object
+
+
+mapaSaur.set("0", "foo");
+mapaSaur.set(1, "bar");
+mapaSaur.set({}, "baz");
+
+console.log(mapaSaur.size);
+console.log(mapaSaur)
+
+for (const entry of mapaSaur) {
+  console.log(entry);
+};
+
+*/
+
+// *****************************************************
+const map = new Map;
+const $cards = [];
+
+console.log($cards)
+
+$cards.appendChild
+
+$cards.push(map);
+
+$cards[0].set(["titulo","Mondongo"])
+
+console.log($cards)
